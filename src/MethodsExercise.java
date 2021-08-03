49528cb644a1e1a95f12f508a28734d6f926d65c
Oscar Santana <oscarsantana2014@gmail.com>
2021-08-03 15:54:54 -0500

    Adding Method EXERCISE

---
+++ src/MethodsExercise.java
@@ -0,0 +1,23 @@
+public class MethodsExercise {
+
+
+
+   public static void AdditionMethod (int number) {
+               number = number + 1;
+               System.out.println("Your final score was " + number);
+
+   }
+
+public static void SubtractionMethod ()
+
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+        AdditionMethod(1);
+    }
+}
--- src/StringExercise.java
+++ src/StringExercise.java
@@ -1,8 +1,19 @@
 public class StringExercise {
     public static void main(String[] args) {
-        String myvar =" We don't need no education " +
+        String myvar =" We don't need no education\n " +
         "We don't need no thought control";
+        System.out.println(myvar);
 
-        System.out.printf(myvar);
+
+        String var2 = "Check\"this\"out!,\"s inside of\"s!";
+        System.out.println(var2);
+
+        String var3 = "in windows, the main drive is usually C:\\";
+        System.out.println(var3);
+
+//        String var 4 = "I can do backslashes" \"\", double backslashes \\,
+//        and the amazing triple backslash \\\!"
+//        System.out.println(var4);
+
     }
 }
