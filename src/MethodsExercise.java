3f74673262c03bc0b3da1e96a9f7a88ba9253d1c
Oscar Santana <oscarsantana2014@gmail.com>
2021-08-04 12:07:15 -0500

    Adding more excercises for Methods

--- src/MethodsExercise.java
+++ src/MethodsExercise.java
@@ -1,23 +1,70 @@
+import java.util.Scanner;
+
 public class MethodsExercise {
+   static Scanner in  = new Scanner(System.in);
+
+    public static int AdditionMethod(int number, int numberTwo) {
+        return (number + numberTwo);
+
+    }
+
+    public static int SubtractionMethod(int number, int numberTwo) {
+        return (number - numberTwo);
+    }
+
+    public static int MultiplicationMethod(int number, int numberTwo) {
+        return (number * numberTwo);
+
+    }
+
+    public static double DivisionMethod(int number, int numberTwo) {
+        return (number / numberTwo);
+    }
+
+    public static int FindModOfTwo(int number, int numberTwo) {
+        return (number % numberTwo);
+    }
+
+
+    public static int getInteger(int min, int max){
+
+       // do {
+            System.out.print("Enter a number between " +  min + " and " +  max + ":");
+           int input = in.nextInt();
+            if (input < min || input>max){
+                System.out.println("Input  not Accepted");
+            return getInteger(min, max);
+
+            }
+     //   } while (input < min || input > max);
+        return input;
+    }
+//public static String getFactorial
+//
+//    public static int CalculateFactorial (int number){
+//        int FactorialNumber = getInteger(1, 10);
+//        for (int i = 0; i <FactorialNumber ; i++) {
+//            System.out.println(i + "! = " + i + "=" + 1);
+//
+//        }
+//    }
+
+
+    public static
 
 
 
-   public static void AdditionMethod (int number) {
-               number = number + 1;
-               System.out.println("Your final score was " + number);
-
-   }
-
-public static void SubtractionMethod ()
 
 
+                public static void main(String[] args) {
+                    System.out.println(AdditionMethod(1,6));
+                    System.out.println(SubtractionMethod(10,5));
+                    System.out.println(MultiplicationMethod(10,6));
+                    System.out.println(DivisionMethod(30,5));
+                    System.out.println(FindModOfTwo(100,50));
+
+                    getInteger(1,10);
 
 
-
-
-
-
-    public static void main(String[] args) {
-        AdditionMethod(1);
     }
 }
