f1ceecf971d107daa16825e3752a722f286229e7
Oscar Santana <oscarsantana2014@gmail.com>
2021-08-10 14:12:42 -0500

    Adding Arrays Exercise

---
+++ src/ArrayExercises.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+
+class ArraysExercises {
+
+
+
+    public static void main(String[] args) {
+
+
+        Person [] people = new Person[3];
+        people[0]= new Person("BobbyHill");
+        people[1]= new Person("Hank");
+        people[2]= new Person("HeisinBerg");
+
+        for (int i = 0; i < people.length; i++) {
+            System.out.println(people[i].getName());
+
+        }
+        people = addPerson(people, new Person ("New Student"));
+        System.out.println();
+        for (Person person: people) {
+            System.out.println(person.getName());
+        }
+
+    }
+
+    public static Person [] addPerson(Person [] people, Person personToAdd){
+        Person[] newPeopleArray = Arrays.copyOf(people, people.length + 1);
+
+        newPeopleArray[newPeopleArray.length -1]= personToAdd;
+        return newPeopleArray;
+
+    }
+}
--- src/Person.java
+++ src/Person.java
@@ -1,50 +1,31 @@
 public class Person {
-    private String name;
-    private int age;
 
-    public String getName() {
-        ////TODO: return the person's name
+    //-----------Object Basics------------
+    private String name;
+
+    public Person(String givenName) {
+        setName(givenName);
+    }
+
+    public String getName(){
         return name;
     }
 
-
-
-    public void setName(String name) {
-        ////TODO: change the name property to the passed value
+    public void setName(String name){
         this.name = name;
     }
 
-public Person(){
-
-}
-
-public Person ( String name, int age){
-        this.name = name;
-        this.age= age;
-}
-public int getAge(){
-            return age;
-}
-
-
-public void  setAge(int age){
-        this.age= age;
-}
     public void sayHello(){
-//TODO: print a message to the console using the person's name
-        System.out.println("Hello friend " +name + " I am " +age+ " years old");
+        System.out.println("Hello," + name);
     }
 
-    public static void main(String[] args) {
-        Person personOne = new Person();
-        personOne.setName("Billy");
-        personOne.setAge(23);
-        System.out.println(personOne.getName());
-        personOne.sayHello();
-       Person persontwo = new Person("Eli",38);
-       persontwo.sayHello();
+    public static void main(String [] args){
 
 
+        Person person1 = new Person("John");
+        Person person2 = new Person("Bob");
+        System.out.println(person1.getName().equals(person2.getName())); //true
+        System.out.println(person1 == person2); //false
 
     }
 }
